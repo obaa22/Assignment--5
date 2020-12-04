df8fa52c86a03dad54f0257678140601f3883e16
obaa22 <74456292+obaa22@users.noreply.github.com>
2020-12-04 15:36:53 +0200

    Add files via upload

--- file 1.cpp
+++ file 1.cpp
@@ -1,26 +1,29 @@
-/*MY Name : Maab Ahmed Omer Ali
-Claas : Second 
+/*My Name : Maab Ahmed Omer Ali 
+Class : second 
 Department : Computer
 */
 #include <iostream>
 using namespace std;
-main ()
+
+int main() {
+int p;
+cin>>p;
+switch (p)
 {
-int p = 3;
-cin p;
-switch (p);
-{
-  case 1:
-    cout<<"Linked list ";
-    break;
-  case 2:
-    cout<<"Stack ";
-    break;
-   case 3;
+    case 1:
+    cout<<"linked list ";
+     break;
+     case 2:
+     cout<<"Stack";
+     break;
+     case 3:
      cout<<"Queue";
-   break;
-  default:
-    cout<<"Is out of oop ";
-    break;
+     break;
+     default:
+     cout<<"is out ";
+     break;
 }
+
+
+    return 0;
 };
