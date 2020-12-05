c83989da4e7500d1619facbbc49b3086b77bb5fd
obaa22 <74456292+obaa22@users.noreply.github.com>
2020-12-05 06:43:22 +0200

    Add files via upload

---
+++ file4.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+int data;
+struct Node *next;
+};
+void push(struct Node**head_ref,int new_data)
+{
+  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node ));
+  new_node->data = new_data;
+  new_node->next = (*head_ref);
+  (*head_ref) = new_node;
+}
+void deleteNode(struct Node**head_ref, int key)
+{
+  struct Node* temp = *head_ref,*prev;
+  if (temp != NULL && temp->data == key);
+  *head_ref = temp->next;
+  free(temp);
+  return;
+while (temp != NULL && temp->data != key) 
+{
+  prev = temp;
+  temp = temp->next;
+}
+if (temp == NULL) return;
+prev->next = temp->next;
+free(temp);
+}
+void printList(struct Node *node)
+{
+  while (node != NULL)
+  {
+    printf(" %d ", node->data);
+    node = node->next;
+  }
+}
+int main()
+{
+  struct Node* head = NULL;
+  push(&head, 4);
+  push(&head, 1);
+  push(&head, 5);
+  push(&head, 8);
+  push(&head, 2);
+  puts("creat Linked List");
+  printList(head);
+  deleteNode(&head, 5);
+  puts("\n  New Linked List ");
+  printList(head);
+  return 0;
+};
+
+
+
+
+
+
+
+
